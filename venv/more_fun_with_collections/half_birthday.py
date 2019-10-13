9394d23660acddec189436fa2fea71152fd9ef97
cfphillips2 <36976465+cfphillips2@users.noreply.github.com>
2019-10-12 22:18:01 -0500

    Wrote function to get half birthday and display info, tests succeeding

--- venv/more_fun_with_collections/half_birthday.py
+++ venv/more_fun_with_collections/half_birthday.py
@@ -1,5 +1,14 @@
+"""Carl Phillips, this program tells you your half birthday"""
+from datetime import datetime, timedelta
+
 def half_birthday(birthday):
-    pass
+    """
+    :param birthday: your birthday
+    :return: your half birthday
+    """
+    half_date = birthday + timedelta(6*365/12)
+    print("Your birthday: " + str(birthday) + "\nYour half birthday: " + str(half_date))
+    return half_date
 
 if __name__ == '__main__':
     half_birthday()
--- venv/test/test_half_birthday.py
+++ venv/test/test_half_birthday.py
@@ -1,11 +1,12 @@
 import unittest
 from unittest.mock import patch
+import datetime
 from more_fun_with_collections import half_birthday
 
 class TestSet(unittest.TestCase):
     def test_half_birthday(self):
         birthday = datetime.datetime(2019, 4, 19)
-        my_half_birthday = datetime.datetime(2019, 10, 19)
+        my_half_birthday = datetime.datetime(2019, 10, 18, 12)
         self.assertEquals(half_birthday.half_birthday(birthday), my_half_birthday)
 
 
