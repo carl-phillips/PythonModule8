bdbf34c5aba0b813ee654785a0c7ef8842269ffe
cfphillips2 <36976465+cfphillips2@users.noreply.github.com>
2019-10-12 20:07:12 -0500

    Wrote tests for the in_dict function. Tests failing

---
+++ venv/more_fun_with_collections/dict_membership.py
@@ -0,0 +1,6 @@
+def in_dict():
+    pass
+
+
+if __name__ == '__main__':
+    in_dict()
---
+++ venv/test/test_dict_membership.py
@@ -0,0 +1,14 @@
+import unittest
+from unittest.mock import patch
+from more_fun_with_collections import dict_membership
+
+class TestSet(unittest.TestCase):
+    def test_in_dict_true(self):
+        self.assertTrue(dic_membership.in_dict({'A':5, 'B':10}))
+
+    def test_in_dict_false(self):
+        self.assertFalse(dict_membership.in_dict({}))
+
+
+if __name__ == '__main__':
+    unittest.main()
